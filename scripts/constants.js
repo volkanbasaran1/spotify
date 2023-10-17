52eb0e55ed146b96adc232333896eafbd09b2528
volkanbasaran1 <volkanba12@gmail.com>
2023-10-17 14:08:19 +0300

    first commit

---
+++ scripts/constants.js
@@ -0,0 +1,8 @@
+export const url = 'https://shazam.p.rapidapi.com/charts/track?locale=TR&pageSize=1000&startFrom=0';
+export const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '43a4bcfacbmsh41fb0954010834bp1b5e4ejsn32f6d78b61d4',
+		'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
+	}
+};
